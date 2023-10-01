2eafecd8e1af4b25e3692e6958e96dbdfb951681
Alex Selyutin <65799598+JuniorQAengineer@users.noreply.github.com>
2023-10-01 14:19:01 +0300

    empty identifier

---
+++ pass_fail.go
@@ -0,0 +1,15 @@
+// reports whether the user has passed the exam
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Print("Enter a grade: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	fmt.Println(input)
+}
