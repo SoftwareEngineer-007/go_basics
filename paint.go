d2a508aec7d59d5bcee567378894de9bdaef2ba8
Alex Selyutin <65799598+JuniorQAengineer@users.noreply.github.com>
2023-10-04 23:03:34 +0300

    function

--- paint.go
+++ paint.go
@@ -2,14 +2,13 @@ package main
 
 import "fmt"
 
+func paitNeeded(width float64, height float64) {
+	area := width * height
+	fmt.Printf("%.2f liters needed\n", area/10.0)
+}
+
 func main() {
-	var width, height, area float64
-	width = 4.2
-	height = 3.0
-	area = width * height
-	fmt.Printf("%.2f liters need\n", area/10.0)
-	width = 5.2
-	height = 3.5
-	area = width * height
-	fmt.Printf("%.2f liters need\n", area/10.0)
+	paitNeeded(4.2, 3.0)
+	paitNeeded(5.2, 3.5)
+	paitNeeded(5.0, 3.3)
 }
