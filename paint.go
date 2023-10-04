f59bb01abe565b4a6be2af630788e1dc8ed05b83
Alex Selyutin <65799598+JuniorQAengineer@users.noreply.github.com>
2023-10-04 23:40:07 +0300

    error processing

--- paint.go
+++ paint.go
@@ -2,13 +2,19 @@ package main
 
 import "fmt"
 
-func paitNeeded(width float64, height float64) {
+func paitNeeded(width float64, height float64) (float64, error) {
+	if width < 0 {
+		return 0, fmt.Errorf("a width of %0.2f is invalid", width)
+	}
+	if height < 0 {
+		return 0, fmt.Errorf("a height of %0.2f is invalid", height)
+	}
 	area := width * height
-	fmt.Printf("%.2f liters needed\n", area/10.0)
+	return area / 10.0, nil
 }
 
 func main() {
-	paitNeeded(4.2, 3.0)
-	paitNeeded(5.2, 3.5)
-	paitNeeded(5.0, 3.3)
+	amount, err := paitNeeded(4.2, -3.0)
+	fmt.Println(err)
+	fmt.Printf("%0.2f liters need\n", amount)
 }
