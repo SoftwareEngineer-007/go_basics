9879ff9a1cc0408e5f68aa683ebd9677609715e5
Alex Selyutin <65799598+JuniorQAengineer@users.noreply.github.com>
2023-10-08 13:42:38 +0300

    make changes to Tocelsius

--- tocelsius.go
+++ tocelsius.go
@@ -2,32 +2,14 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"keyboard"
 	"log"
-	"os"
-	"strconv"
-	"strings"
 )
-
-func getFloat() (float64, error) {
-	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		return 0, err
-	}
-
-	input = strings.TrimSpace(input)
-	number, err := strconv.ParseFloat(input, 64)
-	if err != nil {
-		return 0, err
-	}
-	return number, nil
-}
 
 func main() {
 	fmt.Print("Enter a temperature in Fahrenheit: ")
-	fahrenheit, err := getFloat() // вызываем функцию для получения температуры
+	fahrenheit, err := keyboard.GetFloat() // вызываем функцию для получения температуры
 	if err != nil {
 		log.Fatal(err) // если ошибка, выведет сообщение и закроет программу
 	}
