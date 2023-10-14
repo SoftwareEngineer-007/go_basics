1ca42e14be8547fdafbd971cad73c7c9a05501a5
Alex Selyutin <65799598+JuniorQAengineer@users.noreply.github.com>
2023-10-14 16:22:47 +0300

    array completion

--- src/average/average.go
+++ src/average/average.go
@@ -1,13 +1,21 @@
+// average вычисляет среднее значение
 package main
 
-import "fmt"
+import (
+	datafile "command-line-argumentsD:\\Projects\\go_basics\\src\\datafile\\floats.go"
+	"fmt"
+	"log"
+)
 
 func main() {
-	numbers := [3]float64{71.8, 56.2, 89.5}
+	numbers, err := datafile.GetFloats("data.txt")
+	if err != nill {
+		log.Fatal(err)
+	}
 	var sum float64 = 0
 	for _, number := range numbers {
 		sum += number
 	}
 	sampleCount := float64(len(numbers))
-	fmt.Println("Average: %0.2f\n", sum/sampleCount)
+	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
 }
