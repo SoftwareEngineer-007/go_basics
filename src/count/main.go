1bcacd3c410c0f13be6c72a57016b2a2a59b2006
Alex Selyutin <65799598+JuniorQAengineer@users.noreply.github.com>
2023-10-19 23:06:35 +0300

    adding comments

--- src/count/main.go
+++ src/count/main.go
@@ -12,22 +12,22 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	var names []string
-	var counts []int
+	var names []string // хранение сегмента с именами кандидатов
+	var counts []int   // сегмент с кол-вом вхождений каждого имени
 	for _, line := range lines {
 		matched := false
-		for i, name := range names {
-			if name == line {
-				counts[i]++
-				matched = true
+		for i, name := range names { // перебор из сегмента names
+			if name == line { // если совпадает с текущим именем...
+				counts[i]++    // увеличивает счетчик
+				matched = true // устанавливаем признак обнаруженного совпадения
 			}
 		}
-		if matched == false {
-			names = append(names, line)
-			counts = append(counts, 1)
+		if matched == false { // если совпадение не найдено...
+			names = append(names, line) // добавить его как новое имя в сегмент...
+			counts = append(counts, 1)  // и добавить новый счетчик
 		}
 	}
 	for i, name := range names {
-		fmt.Printf("%s: %d\n", name, counts[i])
+		fmt.Printf("%s: %d\n", name, counts[i]) // вывод каждого элемента names и counts
 	}
 }
