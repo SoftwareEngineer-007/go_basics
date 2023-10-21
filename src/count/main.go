af259a7b9321a94e62d8cbd726d37716b010e668
Alex Selyutin <65799598+JuniorQAengineer@users.noreply.github.com>
2023-10-22 00:30:14 +0300

    map

--- src/count/main.go
+++ src/count/main.go
@@ -12,22 +12,9 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	var names []string // хранение сегмента с именами кандидатов
-	var counts []int   // сегмент с кол-вом вхождений каждого имени
+	counts := make(map[string]int) // объявляем карту
 	for _, line := range lines {
-		matched := false
-		for i, name := range names { // перебор из сегмента names
-			if name == line { // если совпадает с текущим именем...
-				counts[i]++    // увеличивает счетчик
-				matched = true // устанавливаем признак обнаруженного совпадения
-			}
-		}
-		if matched == false { // если совпадение не найдено...
-			names = append(names, line) // добавить его как новое имя в сегмент...
-			counts = append(counts, 1)  // и добавить новый счетчик
-		}
+		counts[line]++ // увеличиваем счетчик голосов
 	}
-	for i, name := range names {
-		fmt.Printf("%s: %d\n", name, counts[i]) // вывод каждого элемента names и counts
-	}
+	fmt.Println(counts) // выводит заполненную карту
 }
