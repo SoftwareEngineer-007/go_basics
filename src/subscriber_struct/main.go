a630f6f8973421060f62d9466cce778af82f1d7e
Alex Selyutin <65799598+JuniorQAengineer@users.noreply.github.com>
2023-10-24 23:09:01 +0300

    structures and pointers

--- src/subscriber_struct/main.go
+++ src/subscriber_struct/main.go
@@ -8,23 +8,27 @@ type subscriber struct {
 	active bool
 }
 
-func printInfo(s subscriber) { // Объявляется один параметр с типом "subskriber"
+func printInfo(s *subscriber) { // Объявляется один параметр с типом "subskriber"
 	fmt.Println("Name:", s.name)
 	fmt.Println("Monthly rate:", s.rate)
 	fmt.Println("Active?", s.active)
 }
 
-func defaultSubscriber(name string) subscriber { //возвращает значение "subskriber"
+func defaultSubscriber(name string) *subscriber { // возвращает указатель
 	var s subscriber // создается новое значение "subskriber"
 	s.name = name
 	s.rate = 5.99
 	s.active = true
-	return s // возвращает "subskriber"
+	return &s // возвращает указатель на структуру
+}
+
+func applyDiscount(s *subscriber) {
+	s.rate = 4.99
 }
 
 func main() {
-	subscriber1 := defaultSubscriber("Aman Singh")
-	subscriber1.rate = 4.99
+	subscriber1 := defaultSubscriber("Aman Singh") // не структура, а указатель на структуру
+	applyDiscount(subscriber1)
 	printInfo(subscriber1)
 
 	subscriber2 := defaultSubscriber("Beth Ryan")
