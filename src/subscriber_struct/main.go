f05c1e47926aceb0866010d8a3d767b6f518c0f5
Alex Selyutin <65799598+JuniorQAengineer@users.noreply.github.com>
2023-10-26 16:58:42 +0300

    Built-in internal structures

--- src/subscriber_struct/main.go
+++ src/subscriber_struct/main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-	magazine "command-line-argumentsD:\\Projects\\go_basics\\src\\magazine\\magazine.go"
 	"fmt"
+	"src/magazine"
 )
 
 func main() {
 	subscriber := magazine.Subscriber{Name: "Aman Singh"}
-	subscriber.Address.Street = "123 Oak St"
-	subscriber.Address.City = "Omaha"
-	subscriber.Address.State = "NE"
-	subscriber.Address.PostalCode = "68111"
+	subscriber.Street = "123 Oak St"
+	subscriber.City = "Omaha"
+	subscriber.State = "NE"
+	subscriber.PostalCode = "68111"
 	fmt.Println("Subscriber Name:", subscriber.Name)
-	fmt.Println("Street:", subscriber.Address.Street)
-	fmt.Println("City:", subscriber.Address.City)
-	fmt.Println("State:", subscriber.Address.State)
-	fmt.Println("Postal Code:", subscriber.Address.PostalCode)
+	fmt.Println("Street:", subscriber.Street)
+	fmt.Println("City:", subscriber.City)
+	fmt.Println("State:", subscriber.State)
+	fmt.Println("Postal Code:", subscriber.PostalCode)
 
 	employee := magazine.Employee{Name: "Joy Carr"}
-	employee.Address.Street = "456 Elm St"
-	employee.Address.City = "Portland"
-	employee.Address.State = "OR"
-	employee.Address.PostalCode = "97222"
+	employee.Street = "456 Elm St"
+	employee.City = "Portland"
+	employee.State = "OR"
+	employee.PostalCode = "97222"
 	fmt.Println("Employee Name:", employee.Name)
-	fmt.Println("Street:", employee.Address.Street)
-	fmt.Println("City:", employee.Address.City)
-	fmt.Println("State:", employee.Address.State)
-	fmt.Println("Postal Code:", employee.Address.PostalCode)
+	fmt.Println("Street:", employee.Street)
+	fmt.Println("City:", employee.City)
+	fmt.Println("State:", employee.State)
+	fmt.Println("Postal Code:", employee.PostalCode)
 }
