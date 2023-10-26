550340ecab0379deaa30bd4902e1896507ecc18e
Alex Selyutin <65799598+JuniorQAengineer@users.noreply.github.com>
2023-10-26 16:46:40 +0300

    removing excess debris

--- src/subscriber_struct/main.go
+++ src/subscriber_struct/main.go
@@ -5,30 +5,6 @@ import (
 	"fmt"
 )
 
-type subscriber struct {
-	name   string
-	rate   float64
-	active bool
-}
-
-func printInfo(s *subscriber) { // Объявляется один параметр с типом "subskriber"
-	fmt.Println("Name:", s.name)
-	fmt.Println("Monthly rate:", s.rate)
-	fmt.Println("Active?", s.active)
-}
-
-func defaultSubscriber(name string) *subscriber { // возвращает указатель
-	var s subscriber // создается новое значение "subskriber"
-	s.name = name
-	s.rate = 5.99
-	s.active = true
-	return &s // возвращает указатель на структуру
-}
-
-func applyDiscount(s *subscriber) {
-	s.rate = 4.99
-}
-
 func main() {
 	subscriber := magazine.Subscriber{Name: "Aman Singh"}
 	subscriber.Address.Street = "123 Oak St"
