b61cd64762c68cbabfd2bf841c1ca91c248b79da
Alex Selyutin <65799598+JuniorQAengineer@users.noreply.github.com>
2023-10-29 09:50:24 +0300

    adding new types

--- src/fuel/main.go
+++ src/fuel/main.go
@@ -3,14 +3,23 @@ package main
 import "fmt"
 
 type Liters float64
+type Milliliters float64
 type Gallons float64
 
-func ToGallons(l Liters) Gallons {
+func LitersToGallons(l Liters) Gallons {
 	return Gallons(l * 0.264)
 }
 
-func ToLiters(g Gallons) Liters {
+func MillilitersToGallons(m Milliliters) Gallons {
+	return Gallons(m * 0.000264)
+}
+
+func GallonsToLiters(g Gallons) Liters {
 	return Liters(g * 3.785)
+}
+
+func GallonsToMilliliters(g Gallons) Milliliters {
+	return Milliliters(g * 3785.41)
 }
 
 func main() {
