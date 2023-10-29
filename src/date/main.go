7ada576a105c82daa0d5218a55903fddf4221ff2
Alex Selyutin <65799598+JuniorQAengineer@users.noreply.github.com>
2023-10-29 22:21:40 +0300

    using set methods

--- src/date/main.go
+++ src/date/main.go
@@ -1,48 +1,24 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
+	"src/calendar"
 )
 
-type Date struct {
-	Year  int
-	Month int
-	Day   int
-}
-
-func (d *Date) SetYear(year int) error {
-	if year < 1 {
-		return errors.New("invalid year")
-	}
-	d.Year = year
-	return nil
-}
-
-func (d *Date) SetMonth(month int) error {
-	if month < 1 || month > 12 {
-		return errors.New("invalid month")
-	}
-	d.Month = month
-	return nil
-}
-
-func (d *Date) SetDay(day int) error {
-	if day < 1 || day > 31 {
-		return errors.New("invalid day")
-	}
-	d.Day = day
-	return nil
-}
-
 func main() {
-	date := Date{}
-	err := date.SetYear(2023)
+	date := calendar.Date{}
+	err := date.SetYear(2023) // испоьзуется set-метод
 	if err != nil {
 		log.Fatal(err)
 	}
-	date.SetMonth(10)
-	date.SetDay(29)
+	err = date.SetMonth(10) // испоьзуется set-метод
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = date.SetDay(29) // испоьзуется set-метод
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(date)
 }
