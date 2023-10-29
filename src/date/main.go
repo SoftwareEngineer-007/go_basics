95bc5ef657f903701e1b79f1fbaf04a60c02641c
Alex Selyutin <65799598+JuniorQAengineer@users.noreply.github.com>
2023-10-29 21:38:30 +0300

    compliance check

--- src/date/main.go
+++ src/date/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+)
 
 type Date struct {
 	Year  int
@@ -8,21 +12,36 @@ type Date struct {
 	Day   int
 }
 
-func (d *Date) SetYear(year int) {
+func (d *Date) SetYear(year int) error {
+	if year < 1 {
+		return errors.New("invalid year")
+	}
 	d.Year = year
+	return nil
 }
 
 func (d *Date) SetMonth(month int) {
+	if month < 1 || month > 12 {
+		return errors.New("invalid month")
+	}
 	d.Month = month
+	return nil
 }
 
 func (d *Date) SetDay(day int) {
+	if day < 1 || day > 31 {
+		return errors.New("invalid day")
+	}
 	d.Day = day
+	return nil
 }
 
 func main() {
 	date := Date{}
-	date.SetYear(2023)
+	err := date.SetYear(2023)
+	if err != nil {
+		log.Fatal(err)
+	}
 	date.SetMonth(10)
 	date.SetDay(29)
 	fmt.Println(date)
